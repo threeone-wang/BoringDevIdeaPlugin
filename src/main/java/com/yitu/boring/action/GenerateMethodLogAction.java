5ad4b96475039b0163e9624ff36d1d1ae1d523e9
⚡️ <>
2022-02-09 10:46:21 +0800

    小调整

--- src/main/java/com/yitu/boring/action/GenerateMethodLogAction.java
+++ src/main/java/com/yitu/boring/action/GenerateMethodLogAction.java
@@ -1,21 +1,16 @@
 package com.yitu.boring.action;
 
-import com.yitu.boring.util.LogJsonUtil;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NotNull;
-
 import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
-import com.intellij.psi.PsiDocumentManager;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiMethod;
-import com.intellij.psi.PsiParameter;
-import com.intellij.psi.PsiParameterList;
+import com.intellij.psi.*;
 import com.intellij.util.IncorrectOperationException;
+import com.yitu.boring.util.LogJsonUtil;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * author : ⚡️
@@ -34,7 +29,7 @@ public class GenerateMethodLogAction extends PsiElementBaseIntentionAction {
 
     @Override
     public boolean isAvailable(@NotNull Project project, Editor editor, @NotNull PsiElement element) {
-        if (element.getParent() instanceof PsiMethod){
+        if (element.getParent() instanceof PsiMethod) {
             return true;
         }
         return false;
@@ -46,10 +41,10 @@ public class GenerateMethodLogAction extends PsiElementBaseIntentionAction {
         Document document = psiDocumentManager.getDocument(element.getContainingFile());
         PsiMethod psiMethod = (PsiMethod) element.getParent();
         String indentStr = calculateIndentStr(psiMethod, document);
-        insertStartLog(psiMethod,document,indentStr);
+        insertStartLog(psiMethod, document, indentStr);
         psiDocumentManager.doPostponedOperationsAndUnblockDocument(document);
         psiDocumentManager.commitDocument(document);
-        insertFinishLog(psiMethod,document,indentStr);
+        insertFinishLog(psiMethod, document, indentStr);
         FileDocumentManager.getInstance().saveDocument(document);
     }
 
@@ -59,59 +54,63 @@ public class GenerateMethodLogAction extends PsiElementBaseIntentionAction {
         return "generate method log";
     }
 
-    private static void insertStartLog(PsiMethod psiMethod, Document document, String indentStr){
-        String logNonParameterFormat="log.info(\"start %s\");";
-        String logFormat="log.info(\"start %s,%s\",%s);";
+    private static void insertStartLog(PsiMethod psiMethod, Document document, String indentStr) {
+        String logNonParameterFormat = "log.info(\"start %s\");";
+        String logFormat = "log.info(\"start %s,%s\", %s);";
         String startLog;
         // String logFormat="log.info(\"start "+psiMethod.getName();
         PsiParameterList parameterList = psiMethod.getParameterList();
         PsiParameter[] parameters = parameterList.getParameters();
-        if (parameters.length==0){
-            startLog= String.format(logNonParameterFormat, psiMethod.getName());
-        }else if (parameters.length==1){
-            startLog= String.format(logFormat, psiMethod.getName(),parameters[0].getName()+":[{}]", LogJsonUtil.logParameterStr(parameters[0]));
-        }else if (parameters.length>1){
-            StringBuilder parametersLogText=new StringBuilder(parameters[0].getName()+":[{}]");
-            StringBuilder parametersVariableText=new StringBuilder(parameters[0].getName());
+        if (parameters.length == 0) {
+            startLog = String.format(logNonParameterFormat, psiMethod.getName());
+        } else if (parameters.length == 1) {
+            startLog = String.format(logFormat, psiMethod.getName(), parameters[0].getName() + ":[{}]", LogJsonUtil.logParameterStr(parameters[0]));
+        } else if (parameters.length > 1) {
+            StringBuilder parametersLogText = new StringBuilder(parameters[0].getName() + ":[{}]");
+            StringBuilder parametersVariableText = new StringBuilder(parameters[0].getName());
             for (int i = 1; i < parameters.length; i++) {
-                parametersLogText.append(",").append(parameters[i].getName()+":[{}]");
+                parametersLogText.append(",").append(parameters[i].getName() + ":[{}]");
                 parametersVariableText.append(",").append(LogJsonUtil.logParameterStr(parameters[i]));
             }
-            startLog= String.format(logFormat, psiMethod.getName(),parametersLogText,parametersVariableText);
-        }else {
+            startLog = String.format(logFormat, psiMethod.getName(), parametersLogText, parametersVariableText);
+        } else {
             return;
         }
-        document.insertString(psiMethod.getBody().getTextOffset() + 1, "\n"+indentStr+startLog);
+        document.insertString(psiMethod.getBody().getTextOffset() + 1, "\n" + indentStr + startLog);
     }
 
-    private static void insertFinishLog(PsiMethod psiMethod, Document document, String indentStr){
-        String logFormat="log.info(\"finish %s, response:[{}]\",%s);";
+    private static void insertFinishLog(PsiMethod psiMethod, Document document, String indentStr) {
+        String logFormat = "log.info(\"finish %s, response:[{}]\", %s);";
         String returnTypeName = psiMethod.getReturnType().getPresentableText();
-        if (returnTypeName.contains("<")){
-            returnTypeName=returnTypeName.substring(0,returnTypeName.indexOf("<"));
+        if (returnTypeName.contains("<")) {
+            returnTypeName = returnTypeName.substring(0, returnTypeName.indexOf("<"));
         }
-        if (Character.isUpperCase(returnTypeName.charAt(0))){
-            returnTypeName=Character.toLowerCase(returnTypeName.charAt(0))+returnTypeName.substring(1);
+        if (Character.isUpperCase(returnTypeName.charAt(0))) {
+            returnTypeName = Character.toLowerCase(returnTypeName.charAt(0)) + returnTypeName.substring(1);
         }
-        String finishLog= String.format(logFormat, psiMethod.getName(), returnTypeName);
+        String finishLog = String.format(logFormat, psiMethod.getName(), returnTypeName);
+        if ("void".equals(returnTypeName)) {
+            logFormat = "log.info(\"finish %s\");";
+            finishLog = String.format(logFormat, psiMethod.getName());
+        }
         int methodBodyEndOffset = psiMethod.getBody().getTextRange().getEndOffset();
         String text;
         do {
             methodBodyEndOffset--;
-            text=document.getText(new TextRange(methodBodyEndOffset,methodBodyEndOffset+1));
-        }while (!text.equals("\n"));
-        document.insertString(methodBodyEndOffset, "\n"+indentStr+finishLog);
+            text = document.getText(new TextRange(methodBodyEndOffset, methodBodyEndOffset + 1));
+        } while (!text.equals("\n"));
+        document.insertString(methodBodyEndOffset, "\n" + indentStr + finishLog);
     }
 
-    private static String calculateIndentStr(PsiMethod psiMethod, Document document){
-        int methodStartIndex=psiMethod.getTextRange().getStartOffset();
-        int indentOffset=0;
+    private static String calculateIndentStr(PsiMethod psiMethod, Document document) {
+        int methodStartIndex = psiMethod.getTextRange().getStartOffset();
+        int indentOffset = 0;
         String text;
         do {
             indentOffset++;
-            text=document.getText(new TextRange(methodStartIndex-indentOffset,methodStartIndex-indentOffset+1));
-        }while (!text.equals("\n"));
-        return document.getText(new TextRange(methodStartIndex-indentOffset+1,methodStartIndex))+"    ";
+            text = document.getText(new TextRange(methodStartIndex - indentOffset, methodStartIndex - indentOffset + 1));
+        } while (!text.equals("\n"));
+        return document.getText(new TextRange(methodStartIndex - indentOffset + 1, methodStartIndex)) + "    ";
     }
 
 }
